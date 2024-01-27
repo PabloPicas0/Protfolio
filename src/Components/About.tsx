c501c01ed091d7bfcbd4f5a7c594b6989f977ce1
PabloPicas0 <pawel.fe@gmail.com>
2024-01-27 17:49:57 +0100

    feat: create about component

---
+++ src/Components/About.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about" style={{ scrollMarginTop: "8rem" }}>
+      <p className="about-content">
+        Hi, I'm Paweł, a self-taught programmer from Poland. One day I decided to switch career from being
+        waiter/barman. What I didn't know back then is that in the process of learning to code I would soon
+        discover a true passion.
+      </p>
+
+      <p className="about-content">
+        I have experience developing web applications, building websites and actively contributing to Open
+        Source projects. My world revolves around React - that's the technology I know best and work daily
+        with.
+      </p>
+    </section>
+  );
+};
+
+export default About
