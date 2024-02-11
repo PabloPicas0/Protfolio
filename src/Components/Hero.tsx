0c85ad6ef05a9147a07e64648207566f7c2f009b
PabloPicas0 <pawel.fe@gmail.com>
2024-02-11 22:58:17 +0100

    feat: replace desktop navigation with custom component

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -7,6 +7,7 @@ import useWindowWidth from "../Hooks/useWindowDimensions";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFreeCodeCamp, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import HeroMobile from "./HeroMoblie";
+import Navigation from "./Navigation";
 
 const StyledHeader = styled.header`
   width: 50%;
@@ -27,14 +28,6 @@ const StyledHeader = styled.header`
   }
 `;
 
-const Line = styled.span`
-  display: block;
-  width: 2rem;
-  height: 1px;
-  background-color: rgb(93, 111, 139);
-  transition: all 100ms ease;
-`;
-
 const SocialMedia = styled.ul`
   display: flex;
   gap: 20px;
@@ -43,37 +36,6 @@ const SocialMedia = styled.ul`
   @media (max-width: 1065px) {
     padding: 0;
   }
-`;
-
-const Ul = styled.ul`
-  padding: 0px;
-  margin: 4rem 0rem 0rem 0rem;
-  list-style: none;
-  color: rgb(93, 111, 139);
-
-  li:hover {
-    span {
-      background-color: rgb(215 223 239);
-      width: 4rem;
-    }
-    a {
-      color: rgb(215 223 239);
-    }
-  }
-`;
-
-const Li = styled.li`
-  margin-bottom: 0.75rem;
-  max-width: fit-content;
-`;
-
-const Anchor = styled.a`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  color: inherit;
-  text-decoration: none;
-  letter-spacing: 0.05rem;
 `;
 
 const Hero = (props: { refNavigation: React.MutableRefObject<HTMLAnchorElement | null> }) => {
@@ -97,35 +59,7 @@ const Hero = (props: { refNavigation: React.MutableRefObject<HTMLAnchorElement |
             <p>I build pixel-perfect, accessible products for the web</p>
 
             <nav ref={refNavigation}>
-              <Ul>
-                <Li>
-                  <Anchor href="#about" className="active">
-                    <Line></Line>
-                    About
-                  </Anchor>
-                </Li>
-
-                <Li>
-                  <Anchor href="#major-projects">
-                    <Line></Line>
-                    Major Projects
-                  </Anchor>
-                </Li>
-
-                <Li>
-                  <Anchor href="#all-projects">
-                    <Line></Line>
-                    All projects
-                  </Anchor>
-                </Li>
-
-                <Li>
-                  <Anchor href="#contact">
-                    <Line></Line>
-                    Contact
-                  </Anchor>
-                </Li>
-              </Ul>
+              <Navigation isLineVisible={true} />
             </nav>
           </div>
 
@@ -152,35 +86,7 @@ const Hero = (props: { refNavigation: React.MutableRefObject<HTMLAnchorElement |
       ) : (
         <HeroMobile>
           <nav>
-            <Ul>
-              <Li>
-                <Anchor href="#about" className="active">
-                  <Line></Line>
-                  About
-                </Anchor>
-              </Li>
-
-              <Li>
-                <Anchor href="#major-projects">
-                  <Line></Line>
-                  Major Projects
-                </Anchor>
-              </Li>
-
-              <Li>
-                <Anchor href="#all-projects">
-                  <Line></Line>
-                  All projects
-                </Anchor>
-              </Li>
-
-              <Li>
-                <Anchor href="#contact">
-                  <Line></Line>
-                  Contact
-                </Anchor>
-              </Li>
-            </Ul>
+            <Navigation isLineVisible={false} />
           </nav>
 
           <SocialMedia>
