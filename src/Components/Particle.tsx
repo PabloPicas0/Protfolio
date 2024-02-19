795329a754f868d8347bf351cc9d5206db195b23
PabloPicas0 <pawel.fe@gmail.com>
2024-02-19 22:36:57 +0100

    feat: add props types

--- src/Components/Particle.tsx
+++ src/Components/Particle.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Particles from "@tsparticles/react";
 import { memo } from "react";
+import { ISourceOptions } from "@tsparticles/engine/types/export-types";
 
-const Particle = memo((props) => {
+const Particle = memo((props: { id: string; options: ISourceOptions }) => {
   const { id, options } = props;
 
   return <Particles id={id} options={options} />;
