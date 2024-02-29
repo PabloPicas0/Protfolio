ac16e1fffb9ed79a0b2c682276f67f7c9a922f50
PabloPicas0 <pawel.fe@gmail.com>
2024-02-29 22:42:05 +0100

    refactor: complete change of contact component

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,70 +1,27 @@
-import styled from "@emotion/styled";
-import React, { useState } from "react";
+import React from "react";
 
-const Label = styled.label`
-  margin: 20px 0px;
-`;
-
-const Button = styled.button`
-  margin-top: 20px;
-  padding: 10px 20px;
-  background-color: transparent;
-  border: 1px solid;
-  border-radius: 5px;
-  color: rgba(226, 232, 240, 0.7);
-  cursor: pointer;
-  justify-self: end;
-  font-family: inherit;
-
-  &:hover {
-    color: aquamarine;
-  }
-`;
-
-const Input = styled.input`
-  padding: 5px 10px;
-  border: 1px solid #d3d3d3;
-  font-size: 1rem;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-  font-family: "Inter", sans-serif;
-`;
-
-const TextArea = Input.withComponent("textarea");
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import { Anchor, AnchorsWrapper } from "./ProjectsDetails";
 
 const Contact = (props: { refSections: React.MutableRefObject<HTMLElement[]> }) => {
   const { refSections } = props;
 
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
   return (
     <>
       <section id="contact" ref={(e) => (e ? (refSections.current[3] = e) : e)}>
+        <div style={{ textAlign: "center" }}>
+          <FontAwesomeIcon icon={faPaperPlane} fontSize={"55px"} />
+        </div>
+
         <h2 style={{ textAlign: "center", letterSpacing: "0.05rem" }}>Contact me!</h2>
+        <p>Whether you have an idea for a project or just want to chat, feel free to shoot me an email!</p>
 
-        <form action="#" style={{ display: "grid" }}>
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            type="email"
-            required
-            placeholder="tom@gamil.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <Label htmlFor="message">Message</Label>
-          <TextArea
-            id="message"
-            required
-            placeholder="Hi, how are you today ?"
-            rows={4}
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <Button type="submit">Send</Button>
-        </form>
+        <AnchorsWrapper style={{ justifyContent: "center" }}>
+          <Anchor target="_blank" href="mailto:pawel.fe@gmail.com">
+            Say Hello
+          </Anchor>
+        </AnchorsWrapper>
       </section>
 
       <footer style={{ textAlign: "center", color: "rgba(255,255,255,0.4)" }}>Made by Paweł Feliksiak</footer>
