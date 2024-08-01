ebb411df717807ca51b562faa1fc7d4a65feb049
PabloPicas0 <pawel.fe@gmail.com>
2024-08-01 14:22:09 +0200

    feat: add Titles for about content

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -5,12 +5,14 @@ const About = (props: { refSections: React.MutableRefObject<HTMLElement[]> }) =>
 
   return (
     <section id="about" className="about-section" ref={(e) => (e ? (refSections.current[0] = e) : e)}>
+      <h2>About</h2>
       <p className="about-content">
         Hi, I'm Paweł, a self-taught programmer from Poland. One day I decided to switch career from being
         waiter/barman. What I didn't know back then is that in the process of learning to code I would soon
         discover a true passion.
       </p>
 
+      <h2>What do I do?</h2>
       <p className="about-content">
         I have experience developing web applications, building websites and actively contributing to Open
         Source projects. My world revolves around React - that's the technology I know best and work daily
